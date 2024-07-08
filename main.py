d74faabd01a72584cfebd935d28a4006881e016c
ksg-dev <ksg.dev.data@gmail.com>
2024-07-08 16:47:50 -0500

    add speed test url

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
+# Open speedtest window
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("ENTER URL HERE")
+driver.get("https://www.speedtest.net/")
 
