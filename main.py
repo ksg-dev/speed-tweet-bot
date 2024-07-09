68fee05f6e4998dc3a72b86aee09386b05c04903
ksg-dev <ksg.dev.data@gmail.com>
2024-07-09 11:17:25 -0500

    load twitter logins

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# Use this class to use specific keys besides letters/numb
 from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+TWITTER_EMAIL = os.environ["TWITTER_EMAIL"]
+TWITTER_PASSWORD = os.environ["TWITTER_PASSWORD"]
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
